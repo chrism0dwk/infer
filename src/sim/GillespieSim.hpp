765310f26236b040c863805400099704aecb4396
Chris Jewell <chris.jewell@warwick.ac.uk>
2011-06-20 11:17:03 +0100

    Corrected a bug in GillespieSim where the event queue wasn't ordered properly.  Had forgotten about the extra level of indirection affored by using a queue of pointers for polymorphism.

--- src/sim/GillespieSim.hpp
+++ src/sim/GillespieSim.hpp
@@ -78,6 +78,7 @@ namespace EpiRisk
         ~Event()
         {
         }
+        virtual
         double
         getTime() const
         {
@@ -92,7 +93,7 @@ namespace EpiRisk
         bool
         operator<(Event& rhs)
         {
-          return time_ < rhs.time_;
+          return this->time_ < rhs.getTime();
         }
 
       protected:
@@ -107,14 +108,12 @@ namespace EpiRisk
           Event(pressure->second, time), pressure_(pressure)
         {
         }
-        virtual
-        const typename Model::Individual*
+        virtual const typename Model::Individual*
         getIndividual() const
         {
           return this->individual_;
         }
-        virtual
-        typename PressureCDF::iterator
+        virtual typename PressureCDF::iterator
         getPressureIterator() const
         {
           return pressure_;
@@ -130,7 +129,6 @@ namespace EpiRisk
           Event(individual, time)
         {
         }
-        ;
       };
 
       class Removal : public Event
@@ -140,21 +138,29 @@ namespace EpiRisk
           Event(individual, time)
         {
         }
-        ;
       };
 
-      typedef set<shared_ptr<Event> > EventQueue;
+      struct EventQueueCmp {
+        bool operator() (const shared_ptr<Event>& lhs, const shared_ptr<Event>& rhs) const
+        {
+          return lhs->getTime() < rhs->getTime();
+        }
+      };
+      typedef set<shared_ptr<Event>, EventQueueCmp > EventQueue;
 
       Model& model_;
       Random& random_;
       double maxTime_;
-      double currentTime_;
       PressureCDF pressureCDF_;
       EventQueue eventQueue_;
       typename Model::PopulationType& population_;
+      int numS_, numI_, numN_, numR_;
+      size_t numInfecs_;
 
       void
       calcPressureCDF();
+      void
+      checkPressureCDF() const;
       void
       initEventQueue();
       double
@@ -172,7 +178,8 @@ namespace EpiRisk
 
   template<typename Model>
     GillespieSim<Model>::GillespieSim(Model& model, Random& random) :
-      model_(model), random_(random), population_(model_.getPopulation()),maxTime_(POSINF)
+      model_(model), random_(random), population_(model_.getPopulation()),
+          maxTime_(POSINF),numInfecs_(0)
     {
 
     }
@@ -215,233 +222,280 @@ namespace EpiRisk
     }
 
   template<typename Model>
-  void
-  GillespieSim<Model>::initEventQueue()
-  {
-    eventQueue_.clear();
+    void
+    GillespieSim<Model>::initEventQueue()
+    {
+      eventQueue_.clear();
 
-    // Iterate over individuals infected before currentTime_
-    // and add notification and removal events
-    typename Model::PopulationType::InfectiveIterator stop = population_.infecLowerBound(currentTime_);
-    for(typename Model::PopulationType::InfectiveIterator it = population_.infecBegin();
-        it != stop;
-        it++)
-      {
-        if (it->getN() > currentTime_ && it->getN() < POSINF)
-          {
-            eventQueue_.insert(shared_ptr<Notification>(new Notification(&(*it),it->getN())));
-          }
+      // Iterate over individuals infected before currentTime_
+      // and add notification and removal events
+      typename Model::PopulationType::InfectiveIterator stop =
+          population_.infecLowerBound(population_.getObsTime());
+      for (typename Model::PopulationType::InfectiveIterator it =
+          population_.infecBegin(); it != stop; it++)
+        {
+          if (it->getN() > population_.getObsTime() && it->getN() < POSINF)
+            {
+              eventQueue_.insert(shared_ptr<Notification> (new Notification(
+                  &(*it), it->getN())));
+            }
 
-        if (it->getR() > currentTime_ && it->getR() < POSINF)
-          {
-            eventQueue_.insert(shared_ptr<Removal>(new Removal(&(*it),it->getR())));
-          }
-      }
-  }
+          if (it->getR() > population_.getObsTime() && it->getR() < POSINF)
+            {
+              eventQueue_.insert(shared_ptr<Removal> (new Removal(&(*it),
+                  it->getR())));
+            }
+        }
+    }
 
   template<typename Model>
     double
     GillespieSim<Model>::beta_max() const
     {
-    if(!pressureCDF_.empty()) return (--pressureCDF_.end())->first;
-    else return 0.0;
+      if (!pressureCDF_.empty()) {
+          typename PressureCDF::const_iterator it = pressureCDF_.end();
+          --it;
+          return it->first;
+      }
+      else
+        return 0.0;
     }
 
-
-template<typename Model>
-  void
-  GillespieSim<Model>::infect(Infection* infection)
-  {
-    // Choose notification and removal times
-    double N = infection->getTime() + model_.ItoN(random_);
-    double R = N + model_.NtoR();
-
-    // Set times
-    Events events;
-    events.I = infection->getTime();
-    events.N = N;
-    events.R = R;
-    population_.updateEvents(*(infection->getIndividual()),events);
-
-    // Add infectious pressure to individuals preceeding in the CDF
-    typename PressureCDF::iterator infectee = infection->getPressureIterator();
-
-    double myPressure;
-    if(infectee == pressureCDF_.begin()) myPressure = infectee->first;
-    else myPressure = infectee->first - (--infectee)->first;
-
-    double cumPressure = 0.0;
-
-    typename PressureCDF::iterator it;
-    for(it = pressureCDF_.begin();
-        it != infectee;
-        it++)
-      {
-        cumPressure += model_.beta(*(infection->getIndividual()), *(it->second));
-        const_cast<double&> (it->first) += cumPressure;
-      }
-
-    it = infectee;
-    it++;
-    for(it = it;
-            it != pressureCDF_.end();
-            it++)
-      {
-        cumPressure += model_.beta(*(infection->getIndividual()), *(it->second));
-        const_cast<double&> (it->first) += cumPressure - myPressure;
-      }
-
-    pressureCDF_.erase(infectee);
-
-    // Add notification and removal events to event queue
-    eventQueue_.insert(shared_ptr<Notification>(new Notification(infection->getIndividual(),N)));
-    eventQueue_.insert(shared_ptr<Removal>(new Removal(infection->getIndividual(),R)));
-  }
-
-template<typename Model>
-  void
-  GillespieSim<Model>::notify(Notification* notification)
-  {
-    // Pop the event off the event queue
-    eventQueue_.erase(eventQueue_.begin());
-
-    // Update the pressureCDF
-    double cumPressure = 0.0;
-    for(typename PressureCDF::iterator it = pressureCDF_.begin();
-        it != pressureCDF_.end();
-        it++)
-      {
-        cumPressure += model_.betastar(*(notification->getIndividual()),*(it->second));
-        cumPressure -= model_.beta(*(notification->getIndividual()), *(it->second));
-        const_cast<double&> (it->first) += cumPressure;
-      }
-
-  }
-
-template<typename Model>
-  void
-  GillespieSim<Model>::remove(Removal* removal)
-  {
-    // Pop the event off the event queue
-  eventQueue_.erase(eventQueue_.begin());
-
-  // Update the pressure CDF
-  double cumPressure = 0.0;
-  for(typename PressureCDF::iterator it = pressureCDF_.begin();
-      it != pressureCDF_.end();
-      it++)
+  template<typename Model>
+    void
+    GillespieSim<Model>::infect(Infection* infection)
     {
-      cumPressure -= model_.betastar(*(removal->getIndividual()),*(it->second));
-      const_cast<double&> (it->first) += cumPressure;
-    }
+      numS_--;
+      numI_++;
 
-  }
+      // Choose notification and removal times
+      double N = infection->getTime() + model_.ItoN(random_);
+      double R = N + model_.NtoR();
 
-template<typename Model>
-  void
-  GillespieSim<Model>::simulate()
-  {
+      // Set times
+      Events events;
+      events.I = infection->getTime();
+      events.N = N;
+      events.R = R;
+      if(!population_.updateEvents(*(infection->getIndividual()), events))
+        throw runtime_error("Event time modification failed!");
 
-    // Copy population
-    currentTime_ = model_.getPopulation().getObsTime();
+      // Add infectious pressure to individuals preceeding in the CDF
+      typename PressureCDF::iterator infectee =
+          infection->getPressureIterator();
 
-    // Create the PressureCDF
-    calcPressureCDF();
-    initEventQueue();
+      double myPressure;
+      if (infectee == pressureCDF_.begin())
+        myPressure = infectee->first;
+      else {
+          typename PressureCDF::iterator tmp = infectee;
+          --tmp;
+          myPressure = infectee->first - tmp->first;
+      }
 
-    // Simulate forward until maxTime
-    while (currentTime_ <= maxTime_ && !eventQueue_.empty())
-      {
-        cout << "loop" << endl;
-        // Get next event
-        shared_ptr < Event > event;
+      double cumPressure = 0.0;
 
-        // Time of next infection
-        if(beta_max() < 0) {
-            dumpPressureCDF();
-            throw logic_error("Weirdness in CDF!");
+      typename PressureCDF::iterator it;
+      for (it = pressureCDF_.begin(); it != infectee; it++)
+        {
+          cumPressure += model_.beta(*(infection->getIndividual()),
+              *(it->second));
+          const_cast<double&> (it->first) += cumPressure;
         }
 
-        double nextInfecTime = POSINF;
-        if(!pressureCDF_.empty()) nextInfecTime = currentTime_ + random_.gamma(1, beta_max());
-        cout << "Next infec event time: " << nextInfecTime << endl;
-        // Choose next event
-        if (eventQueue_.begin()->get()->getTime() < nextInfecTime)
-          {
-            event = *(eventQueue_.begin());
-          }
-        else
-          {
-            // Choose infection
-            typename PressureCDF::iterator it = pressureCDF_.upper_bound(
-                random_.uniform(0.0, beta_max()));
-            event = shared_ptr<Infection> (new Infection(it, nextInfecTime));
-          }
+      it = infectee;
+      it++;
+      for (it = it; it != pressureCDF_.end(); it++)
+        {
+          cumPressure += model_.beta(*(infection->getIndividual()),
+              *(it->second));
+          const_cast<double&> (it->first) += cumPressure - myPressure;
+        }
 
-        // 2. Update populations
-        if (Infection* infection = dynamic_cast<Infection*> (event.get()))
-          {
-            cout << "Infection" << endl;
-            infect(infection);
-          }
-        else if (Notification* notification = dynamic_cast<Notification*> (event.get()))
-          {
-            cout << "Notification" << endl;
-            notify(notification);
-          }
-        else if (Removal* removal = dynamic_cast<Removal*> (event.get()))
-          {
-            cout << "Removal" << endl;
-            remove(removal);
-          }
-        else
-          {
-            string msg("Unidentified event: ");
-            msg += typeid(event.get()).name();
-            throw logic_error(msg);
-          }
+      pressureCDF_.erase(infectee);
 
-        currentTime_ = event.get()->getTime();
+      // Add notification and removal events to event queue
+      pair<typename EventQueue::iterator, bool> rv;
+      rv = eventQueue_.insert(shared_ptr<Notification> (new Notification(
+          infection->getIndividual(), events.N)));
+      if (rv.second != true) throw logic_error("Cannot insert notification event!");
 
-      }
+      rv = eventQueue_.insert(shared_ptr<Removal> (new Removal(
+          infection->getIndividual(), events.R)));
+      if(rv.second != true) throw logic_error("Cannot insert removal event!");
 
-  }
-
-template<typename Model>
-void
-GillespieSim<Model>::dumpPressureCDF() const
-{
-  cout << "================= Pressure Dump ==================" << endl;
-  for(typename PressureCDF::const_iterator it = pressureCDF_.begin();
-      it != pressureCDF_.end();
-      it++)
-    {
-      cout << it->first << "\t" << it->second->getId() << endl;
+      numInfecs_++;
     }
-  cout << "==================================================" << endl;
-}
 
-template<typename Model>
+  template<typename Model>
+    void
+    GillespieSim<Model>::notify(Notification* notification)
+    {
+      numI_--;
+      numN_++;
+      // Pop the event off the event queue
+      eventQueue_.erase(eventQueue_.begin());
+
+      // Update the pressureCDF
+      double cumPressure = 0.0;
+      for (typename PressureCDF::iterator it = pressureCDF_.begin(); it
+          != pressureCDF_.end(); it++)
+        {
+          cumPressure += model_.betastar(*(notification->getIndividual()),
+              *(it->second));
+          cumPressure -= model_.beta(*(notification->getIndividual()),
+              *(it->second));
+          const_cast<double&> (it->first) += cumPressure;
+        }
+
+    }
+
+  template<typename Model>
+    void
+    GillespieSim<Model>::remove(Removal* removal)
+    {
+      numN_--;
+      numR_++;
+      // Pop the event off the event queue
+      eventQueue_.erase(eventQueue_.begin());
+
+      // Update the pressure CDF
+      double cumPressure = 0.0;
+      for (typename PressureCDF::iterator it = pressureCDF_.begin(); it
+          != pressureCDF_.end(); it++)
+        {
+          cumPressure -= model_.betastar(*(removal->getIndividual()),
+              *(it->second));
+          const_cast<double&> (it->first) += cumPressure;
+        }
+
+    }
+
+  template<typename Model>
+    void
+    GillespieSim<Model>::simulate()
+    {
+      numS_ = population_.size() - 1;
+      numI_ = 1;
+      numN_ = 0;
+      numR_ = 0;
+      // Copy population
+      double currentTime = population_.getObsTime();
+
+      // Create the PressureCDF
+      calcPressureCDF();
+      initEventQueue();
+
+      // Simulate forward until maxTime
+      while (currentTime <= maxTime_ && !eventQueue_.empty())
+        {
+
+          // Get next event
+          shared_ptr<Event> event;
+
+          // Time of next infection
+          try {
+              checkPressureCDF();
+          }
+          catch (exception& e)
+            {
+              dumpPressureCDF();
+              throw (logic_error&)e;
+            }
+
+          double nextInfecTime = POSINF;
+          if (!pressureCDF_.empty())
+            nextInfecTime = currentTime + random_.gamma(1, beta_max());
+
+          // Choose next event time
+          if ((*eventQueue_.begin())->getTime() < nextInfecTime)
+            {
+              event = *(eventQueue_.begin());
+            }
+          else
+            {
+              // Choose infection
+              typename PressureCDF::iterator it = pressureCDF_.upper_bound(
+                  random_.uniform(0.0, beta_max()));
+              event = shared_ptr<Infection> (new Infection(it, nextInfecTime));
+            }
+
+          currentTime = event->getTime();
+
+          // 2. Update populations
+          if (Infection* infection = dynamic_cast<Infection*> (event.get()))
+            {
+              infect(infection);
+            }
+          else if (Notification* notification = dynamic_cast<Notification*> (event.get()))
+            {
+              notify(notification);
+            }
+          else if (Removal* removal = dynamic_cast<Removal*> (event.get()))
+            {
+              remove(removal);
+            }
+          else
+            {
+              string msg("Unidentified event: ");
+              msg += typeid(event.get()).name();
+              throw logic_error(msg);
+            }
+
+          population_.setObsTime(currentTime);
+
+        }
+
+
+    }
+
+  template<typename Model>
+    void
+    GillespieSim<Model>::dumpPressureCDF() const
+    {
+      cout << "================= Pressure Dump ==================" << endl;
+      for (typename PressureCDF::const_iterator it = pressureCDF_.begin(); it
+          != pressureCDF_.end(); it++)
+        {
+          cout << it->first << "\t" << it->second->getId() << endl;
+        }
+      cout << "==================================================" << endl;
+    }
+
+  template<typename Model>
   void
-  GillespieSim<Model>::serialize(const std::string filename,
-      const bool censored) const
+  GillespieSim<Model>::checkPressureCDF() const
   {
-    ofstream file(filename.c_str());
-    if (!file.is_open()) throw runtime_error("Cannot open file to serialize epidemic output.  Check your file path.");
-
-    // HEADER
-    file << "id,I,N,R\n";
-
-    // CONTENT
-    for(typename Model::PopulationType::InfectiveIterator it = population_.infecBegin();
-        it != population_.infecEnd();
-        it++)
-      {
-        file << it->getId() << "," << it->getI() << "," << it->getN() << "," << it->getR() << "\n";
-      }
-
-    file.close();
+    for (typename PressureCDF::const_iterator it = pressureCDF_.begin(); it
+              != pressureCDF_.end(); it++)
+            {
+              if (it->first <= 0.0) throw logic_error("Pressure <= 0.0!");
+            }
   }
+
+  template<typename Model>
+    void
+    GillespieSim<Model>::serialize(const std::string filename,
+        const bool censored) const
+    {
+      ofstream file(filename.c_str());
+      if (!file.is_open())
+        throw runtime_error(
+            "Cannot open file to serialize epidemic output.  Check your file path.");
+
+      // HEADER
+      file << "id,I,N,R\n";
+
+      // CONTENT
+      for (typename Model::PopulationType::InfectiveIterator it =
+          population_.infecBegin(); it != population_.infecEnd(); it++)
+        {
+          file << it->getId() << "," << it->getI() << "," << it->getN() << ","
+              << it->getR() << "\n";
+        }
+
+      file.close();
+    }
 
 }
 
--- src/unitTests/fmdSim.cpp
+++ src/unitTests/fmdSim.cpp
@@ -20,6 +20,14 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <sstream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
 
 #include "fmdModel.hpp"
 #include "GillespieSim.hpp"
@@ -28,52 +36,175 @@
 
 using namespace EpiRisk;
 
-int main(int argc, char* argv[])
+struct Settings
 {
-  // Simulates from Gillespie sim
-  if (argc != 5) {
-      std::cerr << "Usage: fmdSim <pop file> <epi file> <output prefix> <num sims>\n";
-      return EXIT_FAILURE;
+  string population;
+  string epidemic;
+  string output;
+
+  double minTime, maxTime;
+
+  double ntor;
+
+  double epsilon;
+  double gamma1;
+  double gamma2;
+  double delta;
+  double xi_p, xi_s;
+  double psi_c, psi_p, psi_s;
+  double zeta_p, zeta_s;
+  double phi_c, phi_p, phi_s;
+  double a, b;
+
+  void
+  load(const string& filename)
+  {
+    using boost::property_tree::ptree;
+
+    typedef boost::property_tree::ptree_bad_data bad_data;
+    typedef boost::property_tree::ptree_bad_path bad_xml;
+    typedef boost::property_tree::ptree_error runtime_error;
+    ptree pt;
+
+    read_xml(filename, pt);
+
+    population = pt.get<string> ("fmdGillespieSim.paths.population");
+    epidemic = pt.get<string> ("fmdGillespieSim.paths.epidemic");
+    output = pt.get<string> ("fmdGillespieSim.paths.output");
+
+    minTime = pt.get<double> ("fmdGillespieSim.options.mintime", 0);
+    maxTime = pt.get<double> ("fmdGillespieSim.options.maxtime", POSINF);
+    ntor = pt.get<double> ("fmdGillespieSim.constants.ntor");
+
+    epsilon = pt.get<double> ("fmdGillespieSim.parameters.epsilon");
+    gamma1 = pt.get<double> ("fmdGillespieSim.parameters.gamma1");
+    gamma2 = pt.get<double> ("fmdGillespieSim.parameters.gamma2");
+    delta = pt.get<double> ("fmdGillespieSim.parameters.delta");
+    xi_p = pt.get<double> ("fmdGillespieSim.parameters.xi_p");
+    xi_s = pt.get<double> ("fmdGillespieSim.parameters.xi_s");
+    psi_c = pt.get<double> ("fmdGillespieSim.parameters.psi_c");
+    psi_p = pt.get<double> ("fmdGillespieSim.parameters.psi_p");
+    psi_s = pt.get<double> ("fmdGillespieSim.parameters.psi_s");
+    zeta_p = pt.get<double> ("fmdGillespieSim.parameters.zeta_p");
+    zeta_s = pt.get<double> ("fmdGillespieSim.parameters.zeta_s");
+    phi_c = pt.get<double> ("fmdGillespieSim.parameters.phi_c");
+    phi_p = pt.get<double> ("fmdGillespieSim.parameters.phi_p");
+    phi_s = pt.get<double> ("fmdGillespieSim.parameters.phi_s");
+    a = pt.get<double> ("fmdGillespieSim.parameters.a");
+    b = pt.get<double> ("fmdGillespieSim.parameters.b");
+
   }
 
-  PopDataImporter* popDataImporter = new PopDataImporter(argv[1]);
-  EpiDataImporter* epiDataImporter = new EpiDataImporter(argv[2]);
+};
+
+int
+main(int argc, char* argv[])
+{
+  // Simulates from Gillespie sim
+
+  string configFilename;
+  int seed;
+
+  try
+    {
+      po::options_description desc("Allowed options");
+      desc.add_options()("help,h", "Show help message")("config,c", po::value<
+          string>(), "config file to use")("seed,s", po::value<int>(),
+          "random seed (default 0)");
+
+      po::variables_map vm;
+      po::store(po::parse_command_line(argc, argv, desc), vm);
+      po::notify(vm);
+
+      if (vm.count("help"))
+        {
+          cout << desc << "\n";
+          return EXIT_FAILURE;
+        }
+
+      if (vm.count("config"))
+        {
+          configFilename = vm["config"].as<string> ();
+        }
+      else
+        {
+          cerr << "Config file required" << "\n";
+          cerr << desc << "\n";
+          return EXIT_FAILURE;
+        }
+
+      if (vm.count("seed"))
+        {
+          seed = vm["seed"].as<int> ();
+        }
+      else
+        {
+          seed = 0;
+        }
+    }
+  catch (exception& e)
+    {
+      cerr << "Exception: " << e.what() << "\n";
+      return 2;
+    }
+  catch (...)
+    {
+      cerr << "Unknown exception" << "\n";
+      return 2;
+    }
+
+  // Read in config file
+  Settings settings;
+  try
+    {
+      settings.load(configFilename);
+    }
+  catch (exception& e)
+    {
+      cerr << "Loading config failed: " << e.what() << endl;
+      return EXIT_FAILURE;
+    }
+
+  PopDataImporter* popDataImporter = new PopDataImporter(settings.population);
+  EpiDataImporter* epiDataImporter = new EpiDataImporter(settings.epidemic);
 
   FmdModel::PopulationType myPopulation;
 
   myPopulation.importPopData(*popDataImporter);
   myPopulation.importEpiData(*epiDataImporter);
-  myPopulation.setObsTime(18.0);
+  myPopulation.setObsTime(settings.minTime);
 
   delete popDataImporter;
   delete epiDataImporter;
 
   FmdParameters parameters;
-  parameters.gamma1 = Parameter(0.65,GammaPrior(1,1),"gamma1");
-  parameters.gamma2 = Parameter(1.0,GammaPrior(1,1),"gamma2");
-  parameters.delta = Parameter(1.1,GammaPrior(1,1),"delta");
-  parameters.epsilon = Parameter(3.5,GammaPrior(1,1),"epsilon");
-  parameters.xi_p = Parameter(0.18,GammaPrior(1,1),"xi_p");
-  parameters.xi_s = Parameter(0.13,GammaPrior(1,1),"xi_s");
-  parameters.psi_c = Parameter(1,BetaPrior(2,2),"psi_c");
-  parameters.psi_p = Parameter(1,BetaPrior(2,2),"psi_p");
-  parameters.psi_s = Parameter(1,BetaPrior(2,2),"psi_s");
-  parameters.zeta_p = Parameter(0.14,GammaPrior(1,1),"zeta_p");
-  parameters.zeta_s = Parameter(0.2,GammaPrior(1,1),"zeta_s");
-  parameters.phi_c = Parameter(1,BetaPrior(2,2),"phi_c");
-  parameters.phi_p = Parameter(1,BetaPrior(2,2),"phi_p");
-  parameters.phi_s = Parameter(1,BetaPrior(2,2),"phi_s");
-  parameters.a = Parameter(0.015,GammaPrior(1,1),"a");
-  parameters.b = Parameter(0.8,GammaPrior(1,1),"b");
+  parameters.gamma1 = Parameter(settings.gamma1, GammaPrior(1, 1), "gamma1");
+  parameters.gamma2 = Parameter(settings.gamma2, GammaPrior(1, 1), "gamma2");
+  parameters.delta = Parameter(settings.delta, GammaPrior(1, 1), "delta");
+  parameters.epsilon = Parameter(settings.epsilon, GammaPrior(1, 1), "epsilon");
+  parameters.xi_p = Parameter(settings.xi_p, GammaPrior(1, 1), "xi_p");
+  parameters.xi_s = Parameter(settings.xi_s, GammaPrior(1, 1), "xi_s");
+  parameters.psi_c = Parameter(settings.psi_c, BetaPrior(2, 2), "psi_c");
+  parameters.psi_p = Parameter(settings.psi_p, BetaPrior(2, 2), "psi_p");
+  parameters.psi_s = Parameter(settings.psi_s, BetaPrior(2, 2), "psi_s");
+  parameters.zeta_p = Parameter(settings.zeta_p, GammaPrior(1, 1), "zeta_p");
+  parameters.zeta_s = Parameter(settings.zeta_s, GammaPrior(1, 1), "zeta_s");
+  parameters.phi_c = Parameter(settings.phi_c, BetaPrior(2, 2), "phi_c");
+  parameters.phi_p = Parameter(settings.phi_p, BetaPrior(2, 2), "phi_p");
+  parameters.phi_s = Parameter(settings.phi_s, BetaPrior(2, 2), "phi_s");
+  parameters.a = Parameter(settings.a, GammaPrior(1, 1), "a");
+  parameters.b = Parameter(settings.b, GammaPrior(1, 1), "b");
 
-
-  FmdModel model(myPopulation,parameters);
-  Random random(0);
+  FmdModel model(myPopulation, parameters);
+  Random random(seed);
 
   GillespieSim<FmdModel> simulation(model, random);
+  simulation.setMaxTime(settings.maxTime);
 
   simulation.simulate();
 
-  simulation.serialize("testFile.txt");
-
+  stringstream s(settings.output);
+  s << "." << seed;
+  simulation.serialize(settings.output);
+  return EXIT_SUCCESS;
 }
