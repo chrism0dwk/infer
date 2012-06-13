b63671851bd6cb0ab7be586bf34eee093bfd5e4d
Chris Jewell <chrism0dwk@gmail.com>
2012-06-13 23:34:56 +0100

    Added GPLv3 licence

--- src/Framework/EpiRiskException.hpp
+++ src/Framework/EpiRiskException.hpp
@@ -1,3 +1,22 @@
+/* ./src/Framework/EpiRiskException.hpp
+ *
+ * Copyright 2012 Chris Jewell <chrism0dwk@gmail.com>
+ *
+ * This file is part of InFER.
+ *
+ * InFER is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * InFER is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with InFER.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 /***************************************************************************
  *   Copyright (C) 2010 by Chris Jewell                                    *
  *   chris.jewell@warwick.ac.uk                                            *
