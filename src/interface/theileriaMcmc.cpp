46e40c5b07aac7a9476368199afcc2b5eeef0892
Chris Jewell <cpjewell@massey.ac.nz>
2015-06-03 10:32:19 +0100

    Updated licences and deleted unnecessary source files.

--- src/interface/theileriaMcmc.cpp
+++ src/interface/theileriaMcmc.cpp
@@ -1,28 +1,21 @@
-/*************************************************************************
- *  ./src/unitTests/fmdMcmc.cpp
- *  Copyright Chris Jewell <chrism0dwk@gmail.com> 2012
- *
- *  This file is part of InFER.
- *
- *  InFER is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  InFER is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with InFER.  If not, see <http://www.gnu.org/licenses/>.
- *************************************************************************/
-/*
- * testMcmc.cpp
- *
- *  Created on: Oct 15, 2010
- *      Author: stsiab
- */
+//////////////////////////////////////////////////////////////////////////
+// Copyright 2015 Chris Jewell                                          //
+// 								        //
+// This file is part of nztheileria.                                    //
+//                                                                      //
+// nztheileria is free software: you can redistribute it and/or modify  //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// nztheileria is distributed in the hope that it will be useful,       //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                             			        //
+// You should have received a copy of the GNU General Public License    //
+// along with nztheileria.  If not, see <http://www.gnu.org/licenses/>. //
+//////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <cstdlib>
