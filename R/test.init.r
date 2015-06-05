2489122314a4c691ddccd75a70b233b1618c0a71
Chris Jewell <cpjewell@massey.ac.nz>
2015-06-05 12:47:00 +0100

    Removed unnecessary R package folder.

--- R/test.init.r
+++
@@ -1,8 +0,0 @@
-init <-
-structure(list(epsilon1 = 1, epsilon2 = 1, gamma1 = 4e-05, gamma2 = 1, 
-    xi_2 = 0.4, xi_3 = 11, psi_1 = 0.5, psi_2 = 0.5, psi_3 = 0.5, 
-    zeta_2 = 0.001, zeta_3 = 0.33, phi_1 = 0.5, phi_2 = 0.5, 
-    phi_3 = 0.5, delta = 0.24, a = 4, b = 0.5, alpha = 4), .Names = c("epsilon1", 
-"epsilon2", "gamma1", "gamma2", "xi_2", "xi_3", "psi_1", "psi_2", 
-"psi_3", "zeta_2", "zeta_3", "phi_1", "phi_2", "phi_3", "delta", 
-"a", "b", "alpha"))
